a1fb30b6a2a8c0ff9868e31a959cc4aef680ac59
Nikita Pankratov <rzkn@ya.ru>
2023-12-13 18:57:42 +0300

    env changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,16 @@
 import os
 from pathlib import Path
 
+from decouple import Csv, config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = config('SECRET_KEY_TASKI')
 
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS_TASKI', cast=Csv())
 
 
 # Application definition
